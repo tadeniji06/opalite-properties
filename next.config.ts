6d322b8a23af13a78c18bc2dc214d9132f34aac7
Olutunmise Adeniji <tadeniji06@gmail.com>
2025-12-11 17:47:57 +0100

    0000

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
+	// eslint: {
+	// 	ignoreDuringBuilds: true,
+	// },
 	typescript: {
 		ignoreBuildErrors: true,
+	},
+	images: {
+		unoptimized: true,
 	},
 };
 
